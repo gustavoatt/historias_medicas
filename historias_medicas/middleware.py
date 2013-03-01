8de2a69ae30318a5a5b4b16e39fc78ac3932eec4
Gustavo Torres <gustavoatt@gmail.com>
2013-03-01 16:09:51 +0100

    Middleware was forgotten

--- historias_medicas/middleware.py
+++ historias_medicas/middleware.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponsePermanentRedirect
 
 class SSLMiddleware(object):
 	def process_request(self, request):
@@ -9,4 +9,4 @@ class SSLMiddleware(object):
 		           request.META.get("HTTP_X_FORWARDED_PROTO", "") == 'https']):
 			url = request.build_absolute_uri(request.get_full_path())
 			secure_url = url.replace("http://", "https://")
-			return HttpResponseRedirect(secure_url)
+			return HttpResponsePermanentRedirect(secure_url)
